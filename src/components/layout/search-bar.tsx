18db13a032fe0f5e145a1bc8ad237ab8e7e46307
gombibi <bb.style.on@gmail.com>
2023-06-20 13:31:06 +0900

    검색창 mouseover event 추가, 아이템 클릭 시 isbn 넘기기

--- src/components/layout/search-bar.tsx
+++ src/components/layout/search-bar.tsx
@@ -19,11 +19,13 @@ const SearchBar = () => {
   const [enteredValue, setEnteredValue] = useState('')
   const [bookList, setBookList] = useState<Book[]>([])
 
+  //검색어 입력 시 state update,
   const onChangeValue = (e: React.ChangeEvent<HTMLInputElement>) => {
     setEnteredValue(e.target.value)
-    debouncedOnChangeValue()
+    debouncedSearchBooks()
   }
 
+  //책 리스트 검색
   const searchBooks = () => {
     //TODO:
     //1. enteredValue로 API 에서 검색
@@ -35,7 +37,7 @@ const SearchBar = () => {
       searchedBooks.push({
         id: key,
         isbn: data[key].isbn,
-        imgUrl: data[key].coverSmallUrl,
+        imgUrl: data[key].imgUrl,
         title: data[key].title,
         author: data[key].author,
       })
@@ -49,11 +51,25 @@ const SearchBar = () => {
     setEnteredValue('')
   }
 
+  //TODO: 책 디테일 오픈
+  const openBookDetail = (isbn: string) => {
+    //전달받은 isbn으로 북 디테일 API 조회
+    console.log(isbn)
+  }
+
   //디바운싱 처리 + useMemo()로 함수 메모이제이션
-  const debouncedOnChangeValue = useMemo(() => debounce(searchBooks, 500), [])
+  const debouncedSearchBooks = useMemo(() => debounce(searchBooks, 500), [])
 
   const searchedBookList = bookList.map(book => (
-    <ListItem alignItems='flex-start' sx={{ width: '100%', height: '100%' }}>
+    <ListItem
+      id={book.isbn}
+      alignItems='flex-start'
+      sx={{
+        width: '100%',
+        height: '100%',
+      }}
+      onClick={() => openBookDetail(book.isbn)}
+    >
       <ImageListItem key={book.id} sx={{ mr: 4 }}>
         <img src={book.imgUrl} alt={book.title} style={{ height: 100 }} />
       </ImageListItem>
@@ -127,10 +143,16 @@ const SearchBar = () => {
             top: 80,
             width: '100%',
             maxWidth: 800,
+            maxHeight: 400,
+            overflow: 'auto',
             bgcolor: 'background.paper',
             mt: '0',
             border: 1,
-            borderColor: 'primary.main',
+            borderColor: 'grey.light',
+            '& .MuiListItem-root:hover': {
+              bgcolor: 'primary.light',
+              cursor: 'pointer',
+            },
           }}
         >
           {searchedBookList}
