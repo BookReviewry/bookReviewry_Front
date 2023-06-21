c4cbcdc735ce0b7cbc91ceaf841addda8357d540
hayo01 <yhao9172@gmail.com>
2023-06-21 11:46:26 +0900

    add: EmpathyRate update: change icon_Like

---
+++ src/components/EmpathyRate/EmpathyRate.tsx
@@ -0,0 +1,41 @@
+'use client'
+import React from 'react'
+import { Favorite, FavoriteBorder } from '@mui/icons-material'
+
+/**
+ * 0      : #ffffff
+ * 1-20   : #ffebee
+ * 21-40  : #ffcdd2
+ * 41-60  : #ef9a9a
+ * 61-80 : #e57373
+ * 81-100 : #ef5350
+ */
+type Props = { rate: number }
+
+const EmpathyRate = (props: Props) => {
+  const rateColor = (rate: number) => {
+    if (rate === 0) return
+    if (rate > 0 && rate < 21) return '#ffebee'
+    if (rate > 20 && rate < 41) return '#ffcdd2'
+    if (rate > 40 && rate < 61) return '#ef9a9a'
+    if (rate > 60 && rate < 81) return '#e57373'
+    if (rate > 80) return '#ef5350'
+  }
+
+  return (
+    <>
+      <div style={{ zIndex: 1, display: 'flex', alignItems: 'center' }}>
+        {props.rate === 0 ? (
+          <FavoriteBorder style={{}} />
+        ) : (
+          <Favorite
+            style={{ marginRight: '5px', color: rateColor(props.rate) }}
+          />
+        )}
+        {`${props.rate}%`}
+      </div>
+    </>
+  )
+}
+
+export default EmpathyRate
