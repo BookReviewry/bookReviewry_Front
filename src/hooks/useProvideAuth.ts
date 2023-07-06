5390a3b9d05026d42bbfe2438d35b2962aabd883
gombibi <bb.style.on@gmail.com>
2023-07-06 16:36:17 +0900

    store 위치 변경, 안쓰는 import 요소 삭제

--- src/hooks/useProvideAuth.ts
+++ src/hooks/useProvideAuth.ts
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useLocalStorage } from './useLocalStorage'
 import { Member } from '@/types/type'
 
