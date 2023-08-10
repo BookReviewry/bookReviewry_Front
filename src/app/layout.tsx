1f0c8c6ecc31aa3b45bcb80bddb21b855f90d76c
gombibi <bb.style.on@gmail.com>
2023-08-10 14:23:22 +0900

    metadata api 사용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,9 +4,6 @@ import SideNavigation from '@/components/layout/SideNavigation'
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='ko'>
-      <Head>
-        <meta name='viewport' content='initial-scale=1, width=device-width' />
-      </Head>
       <body>
         <script src='https://accounts.google.com/gsi/client' defer></script>
         <div style={{ display: 'flex', height: '100%', width: '100%', minWidth: 'fit-content' }}>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,13 @@ import '@/assets/styles/globals.scss'
 import RootLayout from '@/app/layout'
 import Main from './main/Main'
 import AuthContextProvider from '../store/AuthContextProvider'
+import { Metadata } from 'next'
+
+//use the Metadata API which automatically handles advanced requirements such as streaming and de-duplicating <head> elements.
+//ref: https://nextjs.org/docs/app/api-reference/functions/generate-metadata
+export const metadata: Metadata = {
+  viewport: 'initial-scale=1, width=device-width',
+}
 
 export default function Home() {
   const [mounted, setMounted] = useState(false)
